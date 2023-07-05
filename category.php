3578ca04a53c0daf6ed2b4fd8f781783c7af0f57
Mateusz <mateuszpodgorny@gmail.com>
2023-07-05 12:06:39 +0200

    add dynamic php generating products in category

--- category.php
+++ category.php
@@ -1,8 +1,8 @@
 <?php 
-// $connect = mysqli_connect('mariadb106.server400620.nazwa.pl', 'server400620_generator2', 'T5%koNsLsw') or die('Could not connect');
-// // $connect = mysqli_connect('sql.server400620.nazwa.pl', 'server400620_generator', '1Zy(opC0d4') or die('Could not connect');
-// mysqli_query($connect,'SET NAMES utf8');
-// mysqli_select_db($connect,'server400620_generator2');
+$connect = mysqli_connect('mariadb106.server400620.nazwa.pl', 'server400620_generator2', 'T5%koNsLsw') or die('Could not connect');
+// $connect = mysqli_connect('sql.server400620.nazwa.pl', 'server400620_generator', '1Zy(opC0d4') or die('Could not connect');
+mysqli_query($connect,'SET NAMES utf8');
+mysqli_select_db($connect,'server400620_generator2');
 
 // $devicesArray = array(
 //     array("chłodziarki farmaceutyczne","",""),
@@ -26,6 +26,7 @@
 //     array("komory fitotronowe","Climatic Chambers With Phytotron System","Cámaras Climáticas Con El Sistema Fitotrón"),
 // ); 
 
+
 /* 
     $device[0] // title_pl
     $device[1] // title_en
@@ -38,6 +39,49 @@
     $device[8] // meta_es
 
 */
+
+$url = $_SERVER['REQUEST_URI'];
+$parts = parse_url($url);
+
+if (isset($parts['path'])) {
+    $path = $parts['path'];
+    // echo "Adres podstrony: " . $path;
+}
+
+if (isset($parts['path'])) {
+    $path = basename($parts['path']);
+}
+
+$lang = "pl";
+$path = "chlodziarki-laboratoryjne";
+// $path = "chlodziarki-farmaceutyczne";
+
+$query = "SELECT name_pl, name_en, name_es, url_pl, url_en, url_es, category_url_pl, category_url_en, category_url_es, seo_description_pl, seo_description_en, seo_description_es, opis_przy_galerii_pl, opis_przy_galerii_en, opis_przy_galerii_es  FROM all_products_list WHERE category_url_".$lang." = '$path'";
+$result = mysqli_query($connect,$query) or die(mysqli_error($connect));
+
+
+$products = array();
+$counter = 0;
+while($row = mysqli_fetch_array($result)) {
+    $products[$counter]['name_pl'] = $row['name_pl'];
+    $products[$counter]['name_en'] = $row['name_en'];
+    $products[$counter]['name_es'] = $row['name_es'];
+    $products[$counter]['url_pl'] = $row['url_pl'];
+    $products[$counter]['url_en'] = $row['url_en'];
+    $products[$counter]['url_es'] = $row['url_es'];
+    $products[$counter]['category_url_pl'] = $row['category_url_pl'];
+    $products[$counter]['category_url_en'] = $row['category_url_en'];
+    $products[$counter]['category_url_es'] = $row['category_url_es'];
+    $products[$counter]['seo_description_pl'] = $row['seo_description_pl'];
+    $products[$counter]['seo_description_en'] = $row['seo_description_en'];
+    $products[$counter]['seo_description_es'] = $row['seo_description_es'];
+    $products[$counter]['opis_przy_galerii_pl'] = $row['opis_przy_galerii_pl'];
+    $products[$counter]['opis_przy_galerii_en'] = $row['opis_przy_galerii_en'];
+    $products[$counter]['opis_przy_galerii_es'] = $row['opis_przy_galerii_es'];
+    $counter++;
+};
+
+
 
 
 // foreach($devicesArray as $key => $device) {
@@ -71,35 +115,26 @@
   </head>
   <body>
     <div class="myContainer">
-            <a href="/">
+            <a href="">
                 <div class="deviceBox categoryLvl2deviceBox">
                     <p class="titleCategoryLvl2Box">Chłodziarki Laboratoryjne</p>
                 </div>
             </a> 
-            <a href="/">
-                <div class="deviceBox categoryLvl2deviceBox">
-                    <div class="devicePhotoBoxLvl2Category" style="background-image: url(./img/zamrazarka-laboratoryjna.webp);"></div>
-                    <div class="titleBarLvl2Category"><p>Chłodziarka Laboratoryjna</p></div>
-                </div>
-            </a>  
-            <a href="/">
-                <div class="deviceBox categoryLvl2deviceBox">
-                    <div class="devicePhotoBoxLvl2Category" style="background-image: url(./img/zamrazarka-laboratoryjna.webp);"></div>
-                    <div class="titleBarLvl2Category"><p>Chłodziarka Laboratoryjna</p></div>
-                </div>
-            </a> 
-            <a href="/">
-                <div class="deviceBox categoryLvl2deviceBox">
-                    <div class="devicePhotoBoxLvl2Category" style="background-image: url(./img/zamrazarka-laboratoryjna.webp);"></div>
-                    <div class="titleBarLvl2Category"><p>Chłodziarka Laboratoryjna</p></div>
-                </div>
-            </a> 
-            <a href="/">
-                <div class="deviceBox categoryLvl2deviceBox">
-                    <div class="devicePhotoBoxLvl2Category" style="background-image: url(./img/zamrazarka-laboratoryjna.webp);"></div>
-                    <div class="titleBarLvl2Category"><p>Chłodziarka Laboratoryjna</p></div>
-                </div>
-            </a> 
+
+            <?php 
+                foreach($products as $key => $device) { ?>
+                    
+                    <a href="<?php echo($_SERVER['HTTP_HOST'].'/'.$device['category_url_'.$lang].'/'.$device['url_'.$lang])?>">
+                        <div class="deviceBox categoryLvl2deviceBox">
+                            <div class="devicePhotoBoxLvl2Category" style="background-image: url(https://pol-eko.com.pl/img/devices_webp_img/pl/<?php echo($device['url_'.$lang]) ?>.webp);"></div>
+                            <div class="titleBarLvl2Category"><p><?php echo($device['name_'.$lang]); ?></p></div>
+                        </div>
+                    </a> 
+                
+            <?php     
+                }
+            ?>
+            
     </div>
     
   </body>
